cea929b360e17ae3b072e07623e9da825eeb59d7
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-08 10:55:07 +0200

    tests and final implementation

--- src/components.ts
+++ src/components.ts
@@ -1,22 +1,33 @@
 import { Logger } from '@w3f/logger';
 import { Downloader } from '@w3f/downloader';
+import fs from 'fs-extra';
+import ospath from 'ospath';
+import path from 'path';
 
-import { Components, ComponentsConfig } from './index';
+import { Components, ComponentsMap } from './index';
 
 
 export class ComponentsManager implements Components {
     constructor(
-        private readonly componentsConfig: ComponentsConfig,
+        private readonly componentsMap: ComponentsMap,
         private readonly donwloader: Downloader,
         private readonly logger: Logger
     ) { }
 
     async path(name: string): Promise<string> {
-        if (!(name in this.componentsConfig)) {
+        if (!(name in this.componentsMap)) {
             throw new Error(`${name} not found in components config`);
         }
-        const url = this.componentsConfig[name];
+        const targetPath = path.join(ospath.data(), 'w3f', 'components', name);
 
-        return "";
+        if (await fs.pathExists(targetPath)) {
+            return targetPath;
+        }
+        const url = this.componentsMap[name];
+        this.logger.info(`Downloading ${url}...`);
+
+        await this.donwloader.getFile(url, targetPath);
+
+        return targetPath;
     }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,7 @@
-export interface ComponentMap {
-    name: string;
-    url: string;
+export interface ComponentsMap {
+    [name: string]: string;
 }
-
-export type ComponentMapList = Array<ComponentMap>;
 
 export interface Components {
     path(componentName: string): Promise<string>;
-}
-
-export interface ComponentsConfig {
-    components: ComponentMapList;
 }
