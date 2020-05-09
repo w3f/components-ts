3e291fce857f6d9ffb6456f9a8eea442a6224215
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-09 19:49:44 +0200

    implement tarball handling

--- src/components.ts
+++ src/components.ts
@@ -3,9 +3,12 @@ import { Downloader, DownloadManager } from '@w3f/downloader';
 import fs from 'fs-extra';
 import ospath from 'ospath';
 import path from 'path';
+import tar from 'tar';
+
 
 import { Components, ComponentsMap } from './index';
 
+const zipExtension = 'tar.gz';
 
 export class ComponentsManager implements Components {
     private downloader: Downloader;
@@ -21,16 +24,37 @@ export class ComponentsManager implements Components {
         if (!(name in this.componentsMap)) {
             throw new Error(`${name} not found in components config`);
         }
-        const targetPath = path.join(ospath.data(), 'w3f', 'components', name);
+        const targetPath = path.join(this.basePath(), name);
 
         if (await fs.pathExists(targetPath)) {
             return targetPath;
         }
-        const url = this.componentsMap[name];
-        this.logger.info(`Downloading ${url}...`);
 
-        await this.downloader.getFile(url, targetPath);
+        const url = this.componentsMap[name];
+
+        let destination = targetPath;
+        if (this.isTarball(url)) {
+            destination = path.join(this.basePath(), path.basename(url));
+        }
+
+        this.logger.info(`Downloading ${url}...`);
+        await this.downloader.getFile(url, destination);
+
+        if (this.isTarball(url)) {
+            this.logger.info('Unpacking tarball...');
+            await tar.extract({ file: destination, cwd: path.dirname(destination) });
+        }
+
+        await fs.chmod(targetPath, "0700");
 
         return targetPath;
+    }
+
+    public basePath(): string {
+        return path.join(ospath.data(), 'w3f', 'components');
+    }
+
+    private isTarball(url: string): boolean {
+        return url.substr(-6) === zipExtension;
     }
 }
